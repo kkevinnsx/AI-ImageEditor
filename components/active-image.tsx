421bef54eaeef111c16e9b7c3e01e568689f6db1
kkevinnsx <kevyn13ofc@gmail.com>
2025-01-23 10:39:59 -0300

    creating a comparison mode

--- components/active-image.tsx
+++ components/active-image.tsx
@@ -2,13 +2,17 @@ import { useImageStore } from "@/lib/image-store";
 import { Layer, useLayerStore } from "@/lib/layer-store";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import ImageComparison from "./layers/image-comparison";
 
 export default function ActiveImage(){
     const generating = useImageStore((state) => state.generating);
     const activeLayer = useLayerStore((state) => state.activeLayer);
-    const layers = useLayerStore((state) => state.activeLayer);
-
-    if(!activeLayer.url) return null
+    const layers = useLayerStore((state) => state.layers);
+    const layerComparisonMode = useLayerStore(
+        (state) => state.layerComparisonMode
+    )
+    const comparedLayers = useLayerStore((state) => state.comparedLayers)
+    if(!activeLayer.url && comparedLayers.length === 0) return null
 
     const renderLayer = (layer : Layer) => (
         <div className="relative w-full h-full flex items-center justify-center">
@@ -34,6 +38,17 @@ export default function ActiveImage(){
             )}
         </div>
     )
+
+    if(layerComparisonMode && comparedLayers.length > 0){
+        const comparisonLayers = comparedLayers
+            .map((id) => layers.find((l) => l.id === id)).
+            filter(Boolean) as Layer[]
+        return(
+            <div className="w-full relative h-svh p-24 bg-secondary flex flex-col items-center justify-center">
+                <ImageComparison layers={comparisonLayers}/>
+            </div>
+        )
+    }
 
     return (
         <div className="w-full relative h-svh p-24 bg-secondary flex flex-col items-center justify-center">
